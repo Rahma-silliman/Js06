d4254966d12e887621ea57994add98d5b117fad4
Rahma-silliman <119931759+Rahma-silliman@users.noreply.github.com>
2023-02-16 19:03:45 +0100

    Update randomNumber.js

--- randomNumber.js
+++ randomNumber.js
@@ -1,8 +1,7 @@
 // Écrire une fonction qui génère et retourne un nombre aléatoire entre 1 et 10 
 function randomNumber(){
-var min=1; 
-var max=10;  
-var random = Math.floor(Math.random()*(max - min)) + min; 
+
+var random = Math.floor((Math.random()*10) + 1); 
 return random;
 };
 console.log(randomNumber());
