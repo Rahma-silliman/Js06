1e77b579e3a5b964081a656bee255f98b695004b
Rahma-silliman <119931759+Rahma-silliman@users.noreply.github.com>
2023-02-16 19:02:18 +0100

    Update pattern.js

--- pattern.js
+++ pattern.js
@@ -1,15 +1,13 @@
-// Ecrivez une fonction JavaScript pour construire le modèle suivant, en utilisant une boucle for imbriquée. 
+//Ecrivez une fonction JavaScript pour construire le modèle suivant, en utilisant une boucle for imbriquée. 
 function pattern(){
-for (var i = 0;i<1;i++){  
-  for (var j = 0;j <1; j++){     
-    console.log('*');    
-    console.log('*','*');
-    console.log('*','*','*')
-    console.log('*','*','*','*')
-    console.log('*','*','*','*','*')
-    
+  var etoile = '';
+for (var i = 0;i < 5;i++){  
+  for (var j = 0;j <= i; j++){     
+  etoile += '*';
   }
+  etoile += '\n';
   }
+  console.log(etoile);
 };
 pattern();
 module.expots = pattern;
